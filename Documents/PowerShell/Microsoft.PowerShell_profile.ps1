1f2c423d4262b7043fb99a1b6537772b68facf46
Senhui (Alex) Guo <general@sguo.org>
2026-05-17 01:42:31 +1000

    chore: simplify powershell bootstrap

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,82 +1,27 @@
-function Install-ProfileModule {
-    param(
-        [Parameter(Mandatory)]
-        [string] $Name
-    )
-
-    if (Get-Module -ListAvailable -Name $Name) {
-        return $true
-    }
-
-    if (-not (Get-Command Install-Module -ErrorAction SilentlyContinue)) {
-        Write-Warning "Install-Module is not available; cannot install PowerShell module '$Name'."
-        return $false
-    }
-
-    try {
-        Install-Module -Name $Name -Scope CurrentUser -Repository PSGallery -Force -AllowClobber -ErrorAction Stop
-        return $true
-    } catch {
-        Write-Warning "Failed to install PowerShell module '$Name': $($_.Exception.Message)"
-        return $false
-    }
+if (-not (Get-Module -ListAvailable -Name Terminal-Icons)) {
+    Install-Module -Name Terminal-Icons -Scope CurrentUser -Repository PSGallery -Force
 }
+Import-Module Terminal-Icons
 
-function Import-ProfileModule {
-    param(
-        [Parameter(Mandatory)]
-        [string] $Name
-    )
-
-    if (Install-ProfileModule -Name $Name) {
-        try {
-            Import-Module $Name -ErrorAction Stop
-        } catch {
-            Write-Warning "Failed to import PowerShell module '$Name': $($_.Exception.Message)"
-        }
-    }
+if (-not (Get-Module -ListAvailable -Name posh-git)) {
+    Install-Module -Name posh-git -Scope CurrentUser -Repository PSGallery -Force
 }
+Import-Module posh-git
 
-function Install-WingetCommand {
-    param(
-        [Parameter(Mandatory)]
-        [string] $CommandName,
-
-        [Parameter(Mandatory)]
-        [string] $PackageId
-    )
-
-    if (Get-Command $CommandName -ErrorAction SilentlyContinue) {
-        return $true
-    }
-
-    if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
-        Write-Warning "winget is not available; cannot install '$PackageId'."
-        return $false
-    }
-
-    winget install --id $PackageId --exact --source winget --accept-package-agreements --accept-source-agreements
-    if ($LASTEXITCODE -ne 0) {
-        Write-Warning "winget failed to install '$PackageId'."
-        return $false
-    }
-
-    return [bool](Get-Command $CommandName -ErrorAction SilentlyContinue)
+if (-not (Get-Command fzf -ErrorAction SilentlyContinue)) {
+    winget install --id junegunn.fzf --exact --source winget --accept-package-agreements --accept-source-agreements
 }
-
-Import-ProfileModule -Name Terminal-Icons
-Import-ProfileModule -Name posh-git
-
-Install-WingetCommand -CommandName fzf -PackageId junegunn.fzf | Out-Null
-if (Get-Command fzf -ErrorAction SilentlyContinue) {
-    Import-ProfileModule -Name PSFzf
+if (-not (Get-Module -ListAvailable -Name PSFzf)) {
+    Install-Module -Name PSFzf -Scope CurrentUser -Repository PSGallery -Force
 }
+Import-Module PSFzf
 
-Install-WingetCommand -CommandName oh-my-posh -PackageId JanDeDobbeleer.OhMyPosh | Out-Null
-if (Get-Command oh-my-posh -ErrorAction SilentlyContinue) {
-    oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/paradox.omp.json" | Invoke-Expression
+if (-not (Get-Command oh-my-posh -ErrorAction SilentlyContinue)) {
+    winget install --id JanDeDobbeleer.OhMyPosh --exact --source winget --accept-package-agreements --accept-source-agreements
 }
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/paradox.omp.json" | Invoke-Expression
 
-if (Install-ProfileModule -Name PSReadLine) {
-    Set-PSReadLineOption -PredictionSource HistoryAndPlugin -PredictionViewStyle ListView
+if (-not (Get-Module -ListAvailable -Name PSReadLine)) {
+    Install-Module -Name PSReadLine -Scope CurrentUser -Repository PSGallery -Force
 }
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin -PredictionViewStyle ListView
