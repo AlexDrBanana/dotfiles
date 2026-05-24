d3b2e285dfc7cff61947ea77ce5cd14697c265e8
Senhui (Alex) Guo <general@sguo.org>
2026-05-24 23:15:28 +1000

    Guard environment setup commands to require installed tools

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,12 +1,16 @@
 if (-not (Get-Module -ListAvailable -Name Terminal-Icons)) {
     Install-Module -Name Terminal-Icons -Scope CurrentUser -Repository PSGallery -Force
 }
-Import-Module Terminal-Icons
+if (Get-Module -ListAvailable -Name Terminal-Icons) {
+    Import-Module Terminal-Icons
+}
 
 if (-not (Get-Module -ListAvailable -Name posh-git)) {
     Install-Module -Name posh-git -Scope CurrentUser -Repository PSGallery -Force
 }
-Import-Module posh-git
+if (Get-Module -ListAvailable -Name posh-git) {
+    Import-Module posh-git
+}
 
 if (-not (Get-Command fzf -ErrorAction SilentlyContinue)) {
     winget install --id junegunn.fzf --exact --source winget --accept-package-agreements --accept-source-agreements
@@ -14,19 +18,27 @@ if (-not (Get-Command fzf -ErrorAction SilentlyContinue)) {
 if (-not (Get-Module -ListAvailable -Name PSFzf)) {
     Install-Module -Name PSFzf -Scope CurrentUser -Repository PSGallery -Force
 }
-Import-Module PSFzf
+if (Get-Module -ListAvailable -Name PSFzf) {
+    Import-Module PSFzf
+}
 
 if (-not (Get-Command oh-my-posh -ErrorAction SilentlyContinue)) {
     winget install --id JanDeDobbeleer.OhMyPosh --exact --source winget --accept-package-agreements --accept-source-agreements
 }
-oh-my-posh init pwsh --config paradox | Invoke-Expression
+if (Get-Command oh-my-posh -ErrorAction SilentlyContinue) {
+    oh-my-posh init pwsh --config paradox | Invoke-Expression
+}
 
 if (-not (Get-Module -ListAvailable -Name PSReadLine)) {
     Install-Module -Name PSReadLine -Scope CurrentUser -Repository PSGallery -Force
 }
-Set-PSReadLineOption -PredictionSource HistoryAndPlugin -PredictionViewStyle ListView
+if (Get-Module -ListAvailable -Name PSReadLine) {
+    Set-PSReadLineOption -PredictionSource HistoryAndPlugin -PredictionViewStyle ListView
+}
 
-fnm env --use-on-cd --shell powershell | Out-String | Invoke-Expression
+if (Get-Command fnm -ErrorAction SilentlyContinue) {
+    fnm env --use-on-cd --shell powershell | Out-String | Invoke-Expression
+}
 
 function get_gcloud_secret {
     param([string]$secret)
